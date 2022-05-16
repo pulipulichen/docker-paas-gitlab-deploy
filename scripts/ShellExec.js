aa0b2af11d4f2c424d197b6e2a4805e5f8301197
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-05-16 09:18:50 +0800

    改用git pull形式：修正dockerfile的寫法。Mon May 16 09:18:50 AM CST 2022

--- scripts/ShellExec.js
+++ scripts/ShellExec.js
@@ -10,8 +10,8 @@ module.exports = function (cmd, stderrHandler, errorHandler) {
 
   if (typeof(errorHandler) !== 'function') {
     errorHandler = function (error, reject) {
-      //console.log(`[ERROR]\n${error.message}`)
-      reject(error)
+      console.log(`[ERROR]\n${error.message}`)
+      //reject(error)
       return
     }
   }
