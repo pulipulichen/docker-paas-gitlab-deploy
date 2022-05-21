300f285ee474b4f8a125e4dfe775504c9eed52d6
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-05-21 14:48:32 +0800

    改用git pull形式：有修改就要套用。Sat May 21 02:48:32 PM CST 2022

--- scripts/RunCypress.sh
+++ scripts/RunCypress.sh
@@ -1,2 +1,5 @@
 npm link js-yaml fast-glob
-cypress run --headless --project test
+
+cypress run --headless --project test --spec "test/cypress/integration/index.spec.js"
+cypress run --headless --project test --spec "test/cypress/integration/**/[!app.spec.js][!index.spec.js]*"
+cypress run --headless --project test --spec "test/cypress/integration/app.spec.js"
