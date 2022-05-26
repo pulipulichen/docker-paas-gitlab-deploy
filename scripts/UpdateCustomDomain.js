27d403511347d7fac772f7b7ae8db3ba26840582
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-05-26 16:07:03 +0800

    starter。Thu May 26 04:07:03 PM CST 2022

--- scripts/UpdateCustomDomain.js
+++ scripts/UpdateCustomDomain.js
@@ -82,6 +82,7 @@ async function setCustomDomain({customDomain, REPO, customDomainFilePath}) {
 }
 
 async function main (config) {
+  return true
 
   if (!config || !config.deploy) {
     return false
