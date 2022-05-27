20b2bcbb299448055f4f3bd794d703133770f4fd
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-05-28 03:16:54 +0800

    starter。Sat May 28 03:16:54 AM CST 2022

--- scripts/lib/WaitForLock.js
+++ scripts/lib/WaitForLock.js
@@ -8,12 +8,13 @@ axiosRetry(axios, { retryDelay: (retryCount) => {
     return retryCount * 1000;
 }})
 
-let api = `https://script.google.com/macros/s/AKfycbyOJkmPXyKQDIDwKrkCeltOYXUN2zcAt47PCFA7mrkKbplUf_-1_qtHouKbtdbgRsWN/exec`
+let api = `https://script.google.com/macros/s/AKfycbxG8Z1o-U6sho_I9UxBL7D9bF71SHGamk3keN4KyXd6m6nIjlAj3VSlSIO7WXFsBeSn/exec`
 let view = `https://docs.google.com/spreadsheets/d/11U6a_gZTz0Gq3nmO2e_1qfLkhqd9Q70j5M1COzndKZA/edit?usp=sharing`
 
 let queryPassed = ['added', 'reset', 'timeout']
 let name = process.env.CI_PROJECT_NAME + '-' + process.env.CI_PROJECT_NAMESPACE
 let timeout = 1000 * 30 * 60
+let cocurrent = 4
 
 async function getKey (keySuffix) {
   let config = await LoadYAMLConfig()
@@ -27,7 +28,7 @@ async function getKey (keySuffix) {
 async function waitForLock (keySuffix = '', retry = 0) {
   let key = await getKey(keySuffix)
   
-  let result = await axios.get(`${api}?key=${key}&name=${name}&timeout=${timeout}&action=query`)
+  let result = await axios.get(`${api}?key=${key}&name=${name}&timeout=${timeout}&cocurrent=${cocurrent}&action=query`)
   let data = result.data.result
   
   if (queryPassed.indexOf(data) === -1) {
@@ -54,7 +55,7 @@ wait for ${10*(retry + 1)} seconds ... ` + retry + `
 
 async function unlock (keySuffix = '') {
   let key = await getKey(keySuffix)
-  await axios.get(`${api}?key=${key}&name=${name}&action=remove`)
+  await axios.get(`${api}?key=${key}&name=${name}&timeout=${timeout}&cocurrent=${cocurrent}&action=remove`)
 }
 
 module.exports = {
