683207cf4bde9f50052e13f25e24c31dab5ae84b
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-05-28 21:50:34 +0800

    starter。Sat May 28 09:50:34 PM CST 2022

--- scripts/lib/WaitForLock.js
+++ scripts/lib/WaitForLock.js
@@ -8,7 +8,7 @@ axiosRetry(axios, { retryDelay: (retryCount) => {
     return retryCount * 1000;
 }})
 
-let api = `https://script.google.com/macros/s/AKfycbwH1Ywg-cdkG0b8U9EKdaNxsdSIYklNhtRkb0pyxpcRfJCT6fQvsOEaHncgU9oTvaWe/exec`
+let api = `https://script.google.com/macros/s/AKfycbzUt4lUl8oJed63XHm7qYMy0YMOVoh0zNpi5NVnxpx7RmmEwfIY1aZJNqz2V1yOf_lq/exec`
 let view = `https://docs.google.com/spreadsheets/d/11U6a_gZTz0Gq3nmO2e_1qfLkhqd9Q70j5M1COzndKZA/edit?usp=sharing`
 
 let queryPassed = ['added', 'reset', 'timeout']
