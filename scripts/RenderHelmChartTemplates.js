622effdfd5e3651bdb3307ba07e068e1003dfa40
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-06-02 23:19:44 +0800

    預覽並切割helm。Thu Jun  2 11:19:44 PM CST 2022

--- scripts/RenderHelmChartTemplates.js
+++ scripts/RenderHelmChartTemplates.js
@@ -45,7 +45,7 @@ RenderHelmChartTemplates
   // console.log(`helm template ${process.env.CI_PROJECT_NAME} ${tempDir} --debug >> ${path.join(tempOutputDir, '/output.txt')}`)
   // await ShellExec(`whereis helm`)
   // await ShellExec(`helm template ${process.env.CI_PROJECT_NAME} ${tempDir} --debug >> ${path.join(tempOutputDir, '/output.txt')}`, {verbose: true})
-  const result = await ShellExec(`helm template ${process.env.CI_PROJECT_NAME} ${tempDir} --debug`, {verbose: true, stderrHandler = function (stderr, reject) {
+  const result = await ShellExec(`helm template ${process.env.CI_PROJECT_NAME} ${tempDir} --debug`, {verbose: true, stderrHandler: function (stderr, reject) {
     if (verbose) {
       console.log(`[STDERR] ${stderr}`);
     }
