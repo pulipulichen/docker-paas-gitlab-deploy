db9c497ffd7d51982876296c19762b4ea0bbdfdf
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-06-03 02:51:42 +0800

    預覽並切割helm。Fri Jun  3 02:51:42 AM CST 2022

--- scripts/RenderHelmChartTemplates.js
+++ scripts/RenderHelmChartTemplates.js
@@ -84,7 +84,7 @@ https://helm-playground.com/#
 
     throw result.stderr
   }
-  throw new Error('Please check helm')
+  // throw new Error('Please check helm')
 
 
   // fs.writeFileSync(path.join(tempOutputDir, '/output.txt'), result, 'utf-8')
