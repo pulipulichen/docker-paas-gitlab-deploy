b6ae31414af4b000416b98023f89f5cb6f21af8c
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-06-03 02:31:18 +0800

    預覽並切割helm。Fri Jun  3 02:31:18 AM CST 2022

--- scripts/RenderHelmChartTemplates.js
+++ scripts/RenderHelmChartTemplates.js
@@ -121,14 +121,14 @@ function extractErrorFilePath(message) {
   let pos2 = message.indexOf('/') + 1
   let pos3 = message.indexOf(':') + 1
   let errorFilePath = message.slice(pos2, pos3)
-
+  console.log({errorFilePath})
   return errorFilePath
 }
 
 function getContentFromErrorFile(errorFilePath) {
   let targetFile = path.join(tempOutputDir, errorFilePath)
 
-  return fs.readFileSync(errorFilePath, 'utf8')
+  return fs.readFileSync(targetFile, 'utf8')
 }
 
 async function getErrorYaml (message) {
