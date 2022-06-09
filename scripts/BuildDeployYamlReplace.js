1580257722e33c2c4416416a57f32fba8304485e
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-06-10 02:11:49 +0800

    把參數從APP載入。Fri Jun 10 02:11:49 AM CST 2022

--- scripts/BuildDeployYamlReplace.js
+++ scripts/BuildDeployYamlReplace.js
@@ -1,14 +1,15 @@
 const ShellExec = require('./lib/ShellExec.js')
 const LoadYAMLConfig = require('./lib/LoadYAMLConfig.js')
 const path = require('path')
+const fs = require('fs')
 
 async function main (tempDir) {
   let config = await LoadYAMLConfig()
-  console.log(1)
+  // console.log(1)
 
   let {WORKDIR, USER, CMD, EXPOSE, ENV} = config.environment.app.Dockerfile
 
-  console.log(2, {WORKDIR, USER, CMD, EXPOSE, ENV})
+  // console.log(2, {WORKDIR, USER, CMD, EXPOSE, ENV})
 
   let replaceVariables = {
     PROJECT_NAME: process.env.CI_PROJECT_NAME,
@@ -21,7 +22,7 @@ async function main (tempDir) {
     DOCKERFILE_ENV: JSON.stringify(ENV),
   }
 
-  console.log(3, replaceVariables)
+  // console.log(3, replaceVariables)
 
   let replaceFiles = [
     path.join(tempDir, '/values.yaml'),
@@ -36,7 +37,17 @@ async function main (tempDir) {
     }
   }
 
-  console.log('4', '完成')
+  for (let i = 0; i < replaceFiles.length; i++) {
+    let file = replaceFiles[i]
+    let fileContent = fs.readFileSync(file, 'utf8')
+
+    for (const [key, value] of Object.entries(replaceVariables)) {
+      fileContent = fileContent.replaceAll(`{{ ${key} }}`, value)
+    }
+    fs.writeFileSync(file, fileContent, 'utf8')
+  }
+
+  // console.log('4', '完成')
 }
 
 module.exports = main
