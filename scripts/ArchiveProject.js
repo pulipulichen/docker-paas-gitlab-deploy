8adc142fda3c87c4e2c76bec37fa89ae946a13a0
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-06-11 16:51:36 +0800

    刪除專案。Sat Jun 11 04:51:36 PM CST 2022

--- scripts/ArchiveProject.js
+++ scripts/ArchiveProject.js
@@ -1,6 +1,7 @@
 const RemoveArgoCD = require('./remove/RemoveArgoCD')
 const RemoveDokcerImage = require('./remove/RemoveDokcerImage')
 const RemoveGitRepository = require('./remove/RemoveGitRepository')
+const WaitForLock = require('./lib/WaitForLock')
 
 async function ArchiveProject () {
   await WaitForLock.lock('GitlabToDeploy', async () => {
