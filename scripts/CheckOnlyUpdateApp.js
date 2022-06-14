21ad9251b68965d3214c6793ecffef4404f15a01
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-06-15 03:34:50 +0800

    tag。Wed Jun 15 03:34:50 AM CST 2022

--- scripts/CheckOnlyUpdateApp.js
+++ scripts/CheckOnlyUpdateApp.js
@@ -1,11 +1,11 @@
 const path = require('path')
-const ShellSpawn = require('./lib/ShellSpawn')
+const ShellExec = require('./lib/ShellExec')
 
 const BUILD_DIR = path.join('/builds/', process.env.CI_PROJECT_NAMESPACE, process.env.CI_PROJECT_NAME)
 module.exports = async function () {
   process.chdir(BUILD_DIR)
   
-  const filelist = await ShellSpawn(`files=(\`git diff-tree --no-commit-id --name-only -r ${process.env.CI_COMMIT_SHA})\``)
+  const filelist = await ShellExec(`files=(\`git diff-tree --no-commit-id --name-only -r ${process.env.CI_COMMIT_SHA})\``)
   console.log(filelist)
   throw new Error('test')
 }
